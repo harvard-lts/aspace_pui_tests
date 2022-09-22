a0ace13982d90e1f4e541343eefc7542908bffaa
Hurlyburly2 <douglas_simon@harvard.edu>
2022-09-22 15:46:28 -0400

    more setup

--- test_pui.py
+++ test_pui.py
@@ -1,17 +1,26 @@
 import pytest
 import selenium
+import os
 from selenium import webdriver
 import sys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-driver = webdriver.Chrome('./chromedriver')
+_base_url = os.getenv('BASE_URL')
 
-def test_first():
+def test_first(driver):
 	driver.maximize_window()
-	driver.get("https://www.python.org")
+	driver.get(str(_base_url))
 	title = driver.title
-	assert title == 'Welcome to Python.org'
+	print(title)
+	assert title == 'Hollis Archival Collection Guides | HOLLIS for'
+
+@pytest.fixture(scope='session', autouse=True)
+def driver():
+	# Will be executed before the first test
+	driver = webdriver.Chrome('./drivers/chromedriver')
+	yield driver
+	# Will be executed after the last test
 	driver.__exit__()
 
